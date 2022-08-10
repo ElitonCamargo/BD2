e4e3d12711f7b568903fdf5b459e94f8aedf8727
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2022-08-10 11:01:27 -0300

    Aula dia 10/08/2022

--- 2022-08-10/comandos.sql
+++ 2022-08-10/comandos.sql
@@ -22,3 +22,20 @@ WHERE
 							AND
 							metal.Simbolo = cotacao.FK_Metal_Simbolo
 						);
+-- Selecione a maior cotação histórica de cada METAL:
+SELECT
+	cotacao_metal.*,
+    cotacao.Data
+FROM    
+        (SELECT
+            metal.Nome as 'Metal',
+            MAX(cotacao.ValorKg) as Valor
+        FROM
+            metal,
+            cotacao
+        WHERE
+            metal.Simbolo = cotacao.FK_Metal_Simbolo
+        GROUP BY metal.Nome) as cotacao_metal,
+        cotacao
+ WHERE
+ 	cotacao.ValorKg = cotacao_metal.Valor;
